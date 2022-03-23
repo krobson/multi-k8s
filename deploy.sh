e47f9230cf7cbddf00c7ca7238a5f810ab6c8ef6
Ken Robson <ken@robson.net>
2022-03-23 14:46:39 +0100

    Fixed incorrect docker hub repo name.

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 set -xv
-docker build -t krobson/multi-client:latest -t krobson/multi-client:${GIT_SHA} -f ./client/Dockerfile ./client
-docker build -t krobson/multi-server:latest -t krobson/multi-server:${GIT_SHA} -f ./server/Dockerfile ./server
-docker build -t krobson/multi-worker:latest -t krobson/multi-worker:${GIT_SHA} -f ./worker/DOckerfile ./worker
+docker build -t thesleepingbarber/multi-client:latest -t thesleepingbarber/multi-client:${GIT_SHA} -f ./client/Dockerfile ./client
+docker build -t thesleepingbarber/multi-server:latest -t thesleepingbarber/multi-server:${GIT_SHA} -f ./server/Dockerfile ./server
+docker build -t thesleepingbarber/multi-worker:latest -t thesleepingbarber/multi-worker:${GIT_SHA} -f ./worker/DOckerfile ./worker
 
-docker push krobson/multi-client:latest
-docker push krobson/multi-server:latest
-docker push krobson/multi-worker:latest
-docker push krobson/multi-client:${GIT_SHA}
-docker push krobson/multi-server:${GIT_SHA}
-docker push krobson/multi-worker:${GIT_SHA}
+docker push thesleepingbarber/multi-client:latest
+docker push thesleepingbarber/multi-server:latest
+docker push thesleepingbarber/multi-worker:latest
+docker push thesleepingbarber/multi-client:${GIT_SHA}
+docker push thesleepingbarber/multi-server:${GIT_SHA}
+docker push thesleepingbarber/multi-worker:${GIT_SHA}
 
 kubectl apply -f k8s
 
-kubectl set image deployments/client-deployment client=krobson/multi-client:${GIT_SHA}
-kubectl set image deployments/server-deployment server=krobson/multi-server:${GIT_SHA}
-kubectl set image deployments/worker-deployment worker=krobson/multi-worker:${GIT_SHA}
+kubectl set image deployments/client-deployment client=thesleepingbarber/multi-client:${GIT_SHA}
+kubectl set image deployments/server-deployment server=thesleepingbarber/multi-server:${GIT_SHA}
+kubectl set image deployments/worker-deployment worker=thesleepingbarber/multi-worker:${GIT_SHA}
