92119d737f54b2ab2cdb763da9b1d5b330ffd916
Ken Robson <ken@robson.net>
2022-03-23 13:09:53 +0100

    Added deployment scripts.

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t krobson/multi-client:latest -t krobson/multi-client:${GIT_SHA} -f ./client/Dockerfile ./client
+docker build -t krobson/multi-server:latest -t krobson/multi-server:${GIT_SHA} -f ./server/Dockerfile ./server
+docker build -t krobson/multi-worker:latest -t krobson/multi-worker:${GIT_SHA} -f ./worker/DOckerfile ./worker
+
+docker push krobson/multi-client:latest
+docker push krobson/multi-server:latest
+docker push krobson/multi-worker:latest
+docker push krobson/multi-client:${GIT_SHA}
+docker push krobson/multi-server:${GIT_SHA}
+docker push krobson/multi-worker:${GIT_SHA}
+
+kubectl apply -f k8s
+
+kubectl set image deployments/client-deployment server=krobson/multi-client:${GIT_SHA}
+kubectl set image deployments/server-deployment client=krobson/multi-server:${GIT_SHA}
+kubectl set image deployments/worker-deployment worker=krobson/multi-worker:${GIT_SHA}
